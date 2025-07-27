da67bc1738b7ab458de8d8558b3351a88def94fd
chris-tang13 <825177485@qq.com>
2025-07-27 11:09:28 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/',
+  build: {
+    target: 'esnext' // 添加这行确保使用最新ES特性
+  }
+})
